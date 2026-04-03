51c86e1c945a87fb71633834bc4144b2436463dc
Mdeshpande <msdesh99@gmail.com>
2026-04-03 16:02:54 -0400

    Added maven surefire plugin and cosnoel output

--- src/test/java/arrays/MaxNumberFromSubArrayClass.java
+++ src/test/java/arrays/MaxNumberFromSubArrayClass.java
@@ -23,6 +23,7 @@ public class MaxNumberFromSubArrayClass {
         }
             resultArr[resultIndex++]=result;
             System.out.println(Arrays.toString(resultArr));
+            System.out.println("=========================COnsole OUTPUT=======================");
     }
 
 }
